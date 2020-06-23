993f780282fbefc0886c9ff43eb135fc65a2254d
Maximilian Brandl <acc+git@brandlmax.com>
2020-06-23 19:43:17 +0200

    TablePrototye Pre RESTAPI

---
+++ TablePrototype/Assets/RunwayRestConnect/RunwayConnect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Proyecto26;
+
+public class RunwayConnect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RestClient.Get("http://localhost:8000/data").Then(response => {
+            Debug.Log(response.Text);
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
