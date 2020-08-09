e828c87766cb42b1e53c39814d7b25c20e50e378
Maximilian Brandl <acc+git@brandlmax.com>
2020-08-09 14:17:12 +0200

    Fixed Landingpage

--- ProjectPage/ButterflyintheRoom/src/components/footer.js
+++ ProjectPage/ButterflyintheRoom/src/components/footer.js
@@ -5,15 +5,17 @@ import "../components/footer.css"
 export default function Footer() {
   return (
     <footer>
-      <Image name="imd" type="svg" alt="We love Interactive Media Design" />
-      <nav>
-        <a href="https://aiiot.space/privacy" target="_blank">
-          Privacy
-        </a>
-        <a href="https://aiiot.space/impressum" target="_blank">
-          Impressum
-        </a>
-      </nav>
+      <div class="footerWrapper">
+        <Image name="imd" type="svg" alt="We love Interactive Media Design" />
+        <nav>
+          <a href="https://aiiot.space/privacy" target="_blank">
+            Privacy
+          </a>
+          <a href="https://aiiot.space/impressum" target="_blank">
+            Impressum
+          </a>
+        </nav>
+      </div>
     </footer>
   )
 }
